951e11bc164d5fc066cf83d96dd83cea78a2619f
virtu-alex <alessio.cordari@gmail.com>
2022-06-29 20:39:34 +0200

    added comments on random.js

--- js/random.js
+++ js/random.js
@@ -1,6 +1,7 @@
 /*Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Sommiamo i due numeri
 Dichiariamo chi ha vinto.*/
 
@@ -16,47 +17,63 @@ Dichiariamo chi ha vinto.*/
 
 
 
-
+// ----- FUNZIONE NUMERO RANDOM PER CPU
 function randomNumberFromCpu(min, max) {
-
-    return Math.floor(Math.random() * (max - min) + min)
+    // ----- FORMULA MATH RANDOM
+    return Math.floor(Math.random() * (max - min) + min);
 }
-
-
-// console.log(randomNumberFromCpu(1, 5));
-
-
-const userChoice = prompt("Pari o dispari").toLowerCase();
-const userNumber = parseInt(prompt("Inserisci un numero compreso tra 1 e 5"));
-
 
 let randomCpu = randomNumberFromCpu(1, 5);
-let sum = 0;
-sum = randomCpu + userNumber;
+// Creo delle variabili da poter ri-utilizzare nel codice
+let pari = "pari";
+let dispari = "dispari";
 
-//Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// ----- PROMPT PARI O DISPARI
+let userChoice = prompt("Pari o dispari").toLowerCase();
+
+// ----- PROMPT INSERISCI NUMERO
+let userNumber = parseInt(prompt("Inserisci un numero compreso tra 1 e 5"));
+
+// ----- VARIABILE DI APPOGGIO PER LA SOMMA
+let sum = 0;
+// ----- SOMMA = NUMERO GENERATO RANDOMICAMENTE + PROMPT USER 
+sum = randomCpu + userNumber;
+// FUNZIONE PER DETERMINARE SE IL NUMERO E' DIVISIBILE PER 2 QUINDI TRUE
 function isEven(numero) {
+    // --- VARIABILE FLAG
     let result;
+    // --- SE E' DIVISIBILE PER 2 ALLORA E' = PARI
     if (numero % 2 === 0) {
-        result = true;
+        result = pari;
+        // --- OPPURE SE NON LO E' = DISPARI
     } else {
-        result = false;
+        result = dispari;
     }
+    // --- RETURN
     return result;
 }
-// se l'utente mette pari e esce somma pari ----> hai vinto
-// se l'utente mette dispari e esce somma dispari ---> hai vinto
-// se l'utente mette pari e esce somma dispari ---> hai perso
-// se l'utente mette dispari e esce sommma pari ---> hai perso
-if (userChoice == "pari" && isEven(sum)) {
-    alert('Hai vinto')
-} else if (userChoice == "dispari" && !isEven(sum)) {
-    alert('Hai vinto')
-} else if (userChoice == "pari" && !isEven(sum)) {
-    alert('Hai perso')
-} else if (userChoice == "dispari" && isEven(sum)) {
-    alert('Hai perso')
+
+// ----- se l'utente inserisce pari e il risultato della somma e' pari ----> hai vinto
+// ----- se l'utente inserisce dispari e il risultato della somma e' dispari ----> hai vinto
+// ----- se l'utente inserisce pari e il risultato della somma e' dispari ----> hai perso
+// ----- se l'utente inserisce dispari e il risultato della sommm e'a pari ----> hai perso
+
+// CONTROLLO 
+if (userChoice == pari && isEven(sum)) {
+    alert('Hai vinto');
+    console.log(sum + ` Somma totale`);
+} else if (userChoice == dispari && !isEven(sum)) {
+    alert('Hai vinto');
+    console.log(sum + ` Somma totale`);
+} else if (userChoice == pari && !isEven(sum)) {
+    alert('Hai perso');
+    console.log(sum + ` Somma totale`);
+} else if (userChoice == dispari && isEven(sum)) {
+    alert('Hai perso');
+    console.log(sum + ` Somma totale`);
 }
 
-console.log(isEven(sum));
-console.log(userNumber, randomCpu);
+// ----- STAMPO TUTTO NELLA CONSOLE
+console.log(isEven(sum) + `        true = pari | false = dispari`);
+console.log(userNumber + ` numero utente`);
+console.log(randomCpu + ` numero random cpu`);
